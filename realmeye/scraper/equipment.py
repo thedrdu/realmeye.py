6ab6c00344327ad0ea90957150db135d37e1be30
thedrdu <thedrdu@gmail.com>
2024-03-21 13:48:09 -0400

    fix url formatting for equipment

--- realmeye/scraper/equipment.py
+++ realmeye/scraper/equipment.py
@@ -4,7 +4,7 @@ from realmeye.constants import routes, HTTPResponseError
 
 async def fetch_equipment_page(equipment_name: str, session: aiohttp.ClientSession) -> str:
     """Asynchronously retrieves the target equipment's RealmEye Wiki page."""
-    safe_formatted_equipment_name = quote(equipment_name).replace("'", "_")
+    safe_formatted_equipment_name = quote(equipment_name.replace("'", "-").replace(" ", "-"))
     url = urljoin(routes.base_equipment_url, safe_formatted_equipment_name)
 
     async with session.get(url) as response:
